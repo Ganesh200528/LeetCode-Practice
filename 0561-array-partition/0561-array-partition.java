875029a61ad2b8e2d4dedd2c89ed2c6308893fcd
Thatavarthi Venkata Naga Ganesh <thatavarthivnganesh@gmail.com>
2026-08-07 21:08:47 +0530

    Time: 19 ms (10.98%), Space: 49.7 MB (55.74%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int gv = 0;
+        int ans = 0;
+        for(int i = 0 ; i < n-1;i =i+2)
+        {
+            int j = i+1;
+            //System.out.println(nums[i]+" "+nums[j]);  
+            gv = Math.min(nums[i],nums[j]);
+            ans += gv;
+        }
+        return ans;
+    }
+}
