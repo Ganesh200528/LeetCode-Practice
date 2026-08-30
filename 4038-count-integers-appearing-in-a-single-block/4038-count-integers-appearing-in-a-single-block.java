5f6d49be8241c7c5a489102c82b87f3dac244525
Thatavarthi Venkata Naga Ganesh <thatavarthivnganesh@gmail.com>
2026-08-30 21:31:10 +0530

    Time: 1 ms (96.6%), Space: 44 MB (14.56%) - LeetHub

---
+++ 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.java
@@ -0,0 +1,67 @@
+class Solution {
+    public int countSpecialIntegers(int[] nums) {
+
+        int[] blocks = new int[101];
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // First element or new block starts
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                blocks[nums[i]]++;
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            if (blocks[i] == 1) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         HashMap<Integer,Integer> hh = new HashMap<>();
+//         for(int i = 0; i < nums.length;i++)
+//         {
+//             hh.put(nums[i],hh.getOrDefault(nums[i],0)+1);
+//         }
+//         int j = 0;
+//         if(nums.length == 1)
+//         {
+//             return 1;
+//         }
+//         int gg = 0;
+//        while(gg < nums.length)
+//        {
+//           nums[gg] == nums[gg+1]
+//        } 
+//        for(int i :hh.keySet())
+//        {
+//         if(hh.get(i) == 1)
+//         {
+//             j++;
+//         }
+//        }
+//        return j;
+//     }
+// }
