5050a90117c6ad61b1d5737e0e4033794ec38cb2
Thatavarthi Venkata Naga Ganesh <thatavarthivnganesh@gmail.com>
2026-09-01 20:36:29 +0530

    Time: 1 ms (25.28%), Space: 43 MB (34.3%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,30 +1,15 @@
-
 class Solution {
-public String toLowerCase(String s) {
-    String sr=s.toLowerCase();
-    return sr;
+    public String toLowerCase(String s) {
+        StringBuilder ff = new StringBuilder();
+
+for (char jj : s.toCharArray()) {
+    if (jj >= 'A' && jj <= 'Z') {
+        jj += 32;
+    }
+
+    ff.append(jj);
+}
+
+return ff.toString();
     }
 }
-//         StringBuilder  ff = new StringBuilder();
-//        String zz = "abcdefghijklmnopqrstuvwxyz";
-//        HashSet<Character> ss = new HashSet<>();
-//        for(char cc : zz.toCharArray())
-//        {
-//             ss.add(cc);
-//        } 
-//        for(char jj : s.toCharArray())
-//        {
-//             if(ss.contains(jj))
-//             {
-//                 ff.append(jj);
-//             }
-//             else
-//             {
-//                 if(jj)
-//                  jj +=32;
-//                 ff.append(jj);
-//             }
-//        }
-//        return ff.toString();
-//     }
-// }
