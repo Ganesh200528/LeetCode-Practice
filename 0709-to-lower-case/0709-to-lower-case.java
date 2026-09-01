c9a7ed0562b022a589d544fbe06b41ee17148fc3
Thatavarthi Venkata Naga Ganesh <thatavarthivnganesh@gmail.com>
2026-09-01 20:34:40 +0530

    Time: 0 ms (100%), Space: 43 MB (51.75%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,30 @@
+
+class Solution {
+public String toLowerCase(String s) {
+    String sr=s.toLowerCase();
+    return sr;
+    }
+}
+//         StringBuilder  ff = new StringBuilder();
+//        String zz = "abcdefghijklmnopqrstuvwxyz";
+//        HashSet<Character> ss = new HashSet<>();
+//        for(char cc : zz.toCharArray())
+//        {
+//             ss.add(cc);
+//        } 
+//        for(char jj : s.toCharArray())
+//        {
+//             if(ss.contains(jj))
+//             {
+//                 ff.append(jj);
+//             }
+//             else
+//             {
+//                 if(jj)
+//                  jj +=32;
+//                 ff.append(jj);
+//             }
+//        }
+//        return ff.toString();
+//     }
+// }
