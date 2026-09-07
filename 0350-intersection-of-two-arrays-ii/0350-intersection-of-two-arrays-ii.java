76874d9c8c2ce85996c4a8b79eb1043c72558715
Thatavarthi Venkata Naga Ganesh <thatavarthivnganesh@gmail.com>
2026-09-07 21:43:15 +0530

    Time: 42 ms (5.21%), Space: 45.8 MB (9.3%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,3 +1,49 @@
+// import java.util.*;
+
+// class Solution {
+//     public int[] intersect(int[] nums1, int[] nums2) {
+
+//         HashMap<Integer, Integer> hh = new HashMap<>();
+//         HashMap<Integer, Integer> kk = new HashMap<>();
+
+//         ArrayList<Integer> qq = new ArrayList<>();
+
+//         for (int i = 0; i < nums1.length; i++) {
+//             hh.put(nums1[i], hh.getOrDefault(nums1[i], 0) + 1);
+//         }
+
+//         for (int j = 0; j < nums2.length; j++) {
+//             kk.put(nums2[j], kk.getOrDefault(nums2[j], 0) + 1);
+//         }
+
+//         for (Map.Entry<Integer, Integer> ee : hh.entrySet()) {
+
+//             if (kk.containsKey(ee.getKey())) {
+
+//                 int count1 = ee.getValue();
+//                 int count2 = kk.get(ee.getKey());
+
+//                 int min = Math.min(count1, count2);
+
+//                 for (int aa = 0; aa < min; aa++) {
+//                     qq.add(ee.getKey());
+//                 }
+//             }
+//         }
+
+//         int[] vv = new int[qq.size()];
+
+//         for (int zz = 0; zz < qq.size(); zz++) {
+//             vv[zz] = qq.get(zz);
+//         }
+
+//         return vv;
+//     }
+// }
+
+
+
+
 import java.util.*;
 
 class Solution {
@@ -8,29 +54,40 @@ class Solution {
 
         ArrayList<Integer> qq = new ArrayList<>();
 
+        // Count nums1
         for (int i = 0; i < nums1.length; i++) {
             hh.put(nums1[i], hh.getOrDefault(nums1[i], 0) + 1);
         }
 
+        // Count nums2
         for (int j = 0; j < nums2.length; j++) {
             kk.put(nums2[j], kk.getOrDefault(nums2[j], 0) + 1);
         }
 
+        // Find intersection
         for (Map.Entry<Integer, Integer> ee : hh.entrySet()) {
 
-            if (kk.containsKey(ee.getKey())) {
+            for (Map.Entry<Integer, Integer> gg : kk.entrySet()) {
 
-                int count1 = ee.getValue();
-                int count2 = kk.get(ee.getKey());
+                if (ee.getKey().equals(gg.getKey())) {
 
-                int min = Math.min(count1, count2);
+                    if (ee.getValue() > gg.getValue()) {
 
-                for (int aa = 0; aa < min; aa++) {
-                    qq.add(ee.getKey());
+                        for (int aa = 0; aa < gg.getValue(); aa++) {
+                            qq.add(ee.getKey());
+                        }
+
+                    } else {
+
+                        for (int aa = 0; aa < ee.getValue(); aa++) {
+                            qq.add(ee.getKey());
+                        }
+                    }
                 }
             }
         }
 
+        // Create array AFTER qq is filled
         int[] vv = new int[qq.size()];
 
         for (int zz = 0; zz < qq.size(); zz++) {
@@ -40,11 +97,6 @@ class Solution {
         return vv;
     }
 }
-
-
-
-
-
 
 
 
