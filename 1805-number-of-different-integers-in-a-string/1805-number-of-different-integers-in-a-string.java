83e3eb5c44ca4deb92e8e5bd31adb015103e285a
Thatavarthi Venkata Naga Ganesh <thatavarthivnganesh@gmail.com>
2026-09-20 15:08:41 +0530

    Time: 5 ms (53.3%), Space: 43.1 MB (61.86%) - LeetHub

--- 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.java
+++ 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.java
@@ -1,45 +1,42 @@
-class Solution {
-    public int numDifferentIntegers(String word) {
-
-        HashSet<String> hh = new HashSet<>();
-        StringBuilder ss = new StringBuilder();
-
-        for (int i = 0; i < word.length(); i++) {
-
-            if (Character.isDigit(word.charAt(i))) {
-                ss.append(word.charAt(i));
-            } 
-            else {
-                if (!ss.isEmpty()) {
-
-                    // Remove leading zeros
-                    String num = ss.toString().replaceFirst("^0+", "");
-
-                    // If number is only zeros
-                    if (num.isEmpty()) {
-                        num = "0";
+class Solution { 
+    public int numDifferentIntegers(String word) { 
+ 
+        HashSet<String> hh = new HashSet<>(); 
+        ArrayList<Character> aa = new ArrayList<>(); 
+ 
+        for (int i = 0; i <= 9; i++) { 
+            aa.add((char)('0' + i)); 
+        } 
+ 
+        StringBuilder ss = new StringBuilder(); 
+ 
+        for (int i = 0; i < word.length(); i++) { 
+ 
+            if (aa.contains(word.charAt(i))) { 
+                ss.append(word.charAt(i)); 
+            }  
+            else { 
+                if (!ss.isEmpty()) { 
+                    while (ss.length() > 1 && ss.charAt(0) == '0') {
+                        ss.deleteCharAt(0);
                     }
 
-                    hh.add(num);
-                    ss.setLength(0);
-                }
-            }
-        }
-
-        // Process last number
-        if (!ss.isEmpty()) {
-
-            String num = ss.toString().replaceFirst("^0+", "");
-
-            if (num.isEmpty()) {
-                num = "0";
+                    hh.add(ss.toString()); 
+                    ss.setLength(0); 
+                } 
+            } 
+        } 
+ 
+        if (!ss.isEmpty()) { 
+            while (ss.length() > 1 && ss.charAt(0) == '0') {
+                ss.deleteCharAt(0);
             }
 
-            hh.add(num);
-        }
-
-        return hh.size();
-    }
+            hh.add(ss.toString()); 
+        } 
+ 
+        return hh.size(); 
+    } 
 }
 
 
