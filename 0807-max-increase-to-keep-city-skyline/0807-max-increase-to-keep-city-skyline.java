2ac183c0d0616910ecf91b3f0ac9c7d7a39b0142
Thatavarthi Venkata Naga Ganesh <thatavarthivnganesh@gmail.com>
2026-09-21 11:26:50 +0530

    Time: 2 ms (8.17%), Space: 45.4 MB (77.38%) - LeetHub

--- 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.java
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.java
@@ -1,43 +1,87 @@
-class Solution
-{
-    public int maxIncreaseKeepingSkyline(int[][] grid) 
-    {
+class Solution {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+
         int n = grid.length;
-        int[] rowMax = new int[n];
-        int[] colMax = new int[n];
-
-        for(int i = 0; i < n;i++)
-        {
-            for(int j = 0; j < n;j++)
-            {
-                rowMax[i] = Math.max(rowMax[i],grid[i][j]);
-            }
-        }
-
-        for(int j = 0; j < n;j++)
-        {
-            for(int i = 0; i < n;i++)
-            {
-                colMax[j] = Math.max(colMax[j],grid[i][j]);
-            }
-        }
-
         int ans = 0;
 
+        for (int i = 0; i < n; i++) {
 
-        for(int i = 0; i < n;i++)
-        {
-            for(int j = 0; j < n;j++)
-            {
-                int newHeight = Math.min(rowMax[i],colMax[j]);
+            for (int j = 0; j < n; j++) {
+
+                int rowMax = 0;
+                int colMax = 0;
+
+                // Find maximum in row
+                for (int k = 0; k < n; k++) {
+                    rowMax = Math.max(rowMax, grid[i][k]);
+                }
+
+                // Find maximum in column
+                for (int k = 0; k < n; k++) {
+                    colMax = Math.max(colMax, grid[k][j]);
+                }
+
+                int newHeight = Math.min(rowMax, colMax);
 
                 ans += newHeight - grid[i][j];
             }
         }
+
         return ans;
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution
+// {
+//     public int maxIncreaseKeepingSkyline(int[][] grid) 
+//     {
+//         int n = grid.length;
+//         int[] rowMax = new int[n];
+//         int[] colMax = new int[n];
+
+//         for(int i = 0; i < n;i++)
+//         {
+//             for(int j = 0; j < n;j++)
+//             {
+//                 rowMax[i] = Math.max(rowMax[i],grid[i][j]);
+//             }
+//         }
+
+//         for(int j = 0; j < n;j++)
+//         {
+//             for(int i = 0; i < n;i++)
+//             {
+//                 colMax[j] = Math.max(colMax[j],grid[i][j]);
+//             }
+//         }
+
+//         int ans = 0;
+
+
+//         for(int i = 0; i < n;i++)
+//         {
+//             for(int j = 0; j < n;j++)
+//             {
+//                 int newHeight = Math.min(rowMax[i],colMax[j]);
+
+//                 ans += newHeight - grid[i][j];
+//             }
+//         }
+//         return ans;
+//     }
+
+// }
 
 
 
