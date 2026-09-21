8ed49f2f99f46ecd8ff7cf9ba3d91e60e6487edb
Thatavarthi Venkata Naga Ganesh <thatavarthivnganesh@gmail.com>
2026-09-21 11:26:06 +0530

    Time: 1 ms (53%), Space: 45.6 MB (42.78%) - LeetHub

---
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.java
@@ -0,0 +1,84 @@
+class Solution
+{
+    public int maxIncreaseKeepingSkyline(int[][] grid) 
+    {
+        int n = grid.length;
+        int[] rowMax = new int[n];
+        int[] colMax = new int[n];
+
+        for(int i = 0; i < n;i++)
+        {
+            for(int j = 0; j < n;j++)
+            {
+                rowMax[i] = Math.max(rowMax[i],grid[i][j]);
+            }
+        }
+
+        for(int j = 0; j < n;j++)
+        {
+            for(int i = 0; i < n;i++)
+            {
+                colMax[j] = Math.max(colMax[j],grid[i][j]);
+            }
+        }
+
+        int ans = 0;
+
+
+        for(int i = 0; i < n;i++)
+        {
+            for(int j = 0; j < n;j++)
+            {
+                int newHeight = Math.min(rowMax[i],colMax[j]);
+
+                ans += newHeight - grid[i][j];
+            }
+        }
+        return ans;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int maxIncreaseKeepingSkyline(int[][] grid) 
+//     {
+//         int ans = 0;
+//         int gg = 0;
+        
+//         for(int i = 0; i < grid.length;i++)
+//         {
+//             int maxx = 0;
+//             int maxxx = 0;
+//             for(int j = 0; j < grid.length;j++)
+//             {
+//                 if(grid[i][j] > maxx)
+//                 {
+//                     maxx = grid[i][j];
+//                 }
+//                 if(grid[j][i] > maxxx)
+//                 {
+//                     maxxx = grid[j][i];
+//                 }
+//             }
+//             gg = Math.max(maxx,maxxx);
+//             ans += gg - grid[i][j]; 
+//         }
+//         return ans;
+
+//     }
+// }
