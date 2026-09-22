b3978249d916ab9da5272c2b52842d00a7e33c3d
Thatavarthi Venkata Naga Ganesh <thatavarthivnganesh@gmail.com>
2026-09-22 18:28:51 +0530

    Time: 1 ms (59.04%), Space: 44.9 MB (87.53%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) 
+    {
+        if(nums.length == 1)
+        {
+            return true;
+        }
+      
+        
+        for(int i = 0; i < nums.length-1;i++)
+        {
+             boolean kk = false;
+            boolean gg = false;
+            if(nums[i] % 2 == 0)
+            {
+                kk = true;
+            }
+            if(nums[i+1] % 2 == 0)
+            {
+                gg = true;
+            }
+            if((kk && gg ) || (!kk && !gg))
+            {
+                return false;
+            }
+        }
+        return true;
+
+         
+    }
+}
