7809dd297f6432163d475c0347733c056f255efd
Thatavarthi Venkata Naga Ganesh <thatavarthivnganesh@gmail.com>
2026-09-23 07:24:16 +0530

    Time: 73 ms (14.35%), Space: 105.4 MB (73.49%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
@@ -1,18 +1,50 @@
 class Solution {
-    public int countDistinctIntegers(int[] nums) 
-    {
-        
+    public int countDistinctIntegers(int[] nums) {
         HashSet<Integer> hh = new HashSet<>();
-        for(int i = 0; i < nums.length;i++)
-        {
+
+        for (int i = 0; i < nums.length; i++) {
             hh.add(nums[i]);
-            StringBuilder ss = new StringBuilder();
-            String kk = String.valueOf(nums[i]);
-            ss.append(nums[i]);
-            ss.reverse();
-            hh.add(Integer.parseInt(ss.toString()));
+
+            int n = nums[i];
+            int rev = 0;
+
+            while (n > 0) {
+                int digit = n % 10;
+                rev = rev * 10 + digit;
+                n = n / 10;
+            }
+
+            hh.add(rev);
         }
-       
+
         return hh.size();
     }
 }
+
+
+
+
+
+
+
+
+
+
+// / class Solution {
+//     public int countDistinctIntegers(int[] nums) 
+//     {
+        
+//         HashSet<Integer> hh = new HashSet<>();
+//         for(int i = 0; i < nums.length;i++)
+//         {
+//             hh.add(nums[i]);
+//             StringBuilder ss = new StringBuilder();
+//             String kk = String.valueOf(nums[i]);
+//             ss.append(nums[i]);
+//             ss.reverse();
+//             hh.add(Integer.parseInt(ss.toString()));
+//         }
+       
+//         return hh.size();
+//     }
+// }
